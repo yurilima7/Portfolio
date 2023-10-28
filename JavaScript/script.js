0843699c28d239a21c934ab8c6c9412c4eaec4b6
Yuri Lima <61335084+yurilima7@users.noreply.github.com>
2023-10-28 19:55:15 -0300

    Atualização do site

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,86 +1,67 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // Verifica se a biblioteca de rolagem suave é suportada pelo navegador
-  if ('scrollBehavior' in document.documentElement.style) {
-    // Se for suportada, não é necessário fazer nada
-  } else {
-    // Se não for suportada, aplica o polyfill para habilitar a funcionalidade
-    smoothscroll.polyfill();
-  }
+    // menu drawer
+    const drawer = document.querySelector('.drawer');
+    const menu = document.querySelector('.menu');
 
-  // Adiciona o evento de clique aos links com a classe "scroll-link"
-  const scrollLinks = document.querySelectorAll('.scroll-link');
-  
-  scrollLinks.forEach(link => {
-    link.addEventListener('click', (event) => {
-      event.preventDefault();
-      const targetId = link.getAttribute('href');
-      const targetSection = document.querySelector(targetId);
-      if (targetSection) {
-        targetSection.scrollIntoView({ behavior: 'smooth' });
-      }
+    drawer.addEventListener('click', () => {
+        menu.classList.toggle('active');
     });
-  });
 
-  const textElement = document.getElementById('typing-text');
-  const subtextElement = document.getElementById('typing-subtext');
-  const text = "Olá, sou o Yuri Lima";
-  const subtext = "Desenvolvedor Mobile";
-  let index = 0;
-
-  function typeEffect() {
-      if (index < text.length) {
-          textElement.textContent += text.charAt(index);
-          index++;
-          setTimeout(typeEffect, 100); // Ajuste o tempo de espera entre cada caractere digitado (em milissegundos)
-      } else {
-        index = 0;
-        typeSubtextEffect();
-      }
-  }
-
-  function typeSubtextEffect() {
-    if (index < subtext.length) {
-        subtextElement.textContent += subtext.charAt(index);
-        index++;
-        setTimeout(typeSubtextEffect, 100);
-    }
-  }
-
-  typeEffect();
-
-  const downloadButton = document.getElementById('download-cv');
-
-  downloadButton.addEventListener('click', () => {
-    const pdfFilePath = 'Docs/Currículo.pdf';
-    const link = document.createElement('a');
-
-    link.href = pdfFilePath;
-    link.download = 'Curriculo.pdf';
-    link.target = '_blank'; // Abrir o link em uma nova aba (opcional)
-
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  });
-
-  const drawer = document.querySelector('.drawer');
-  const menu = document.querySelector('.menu');
-
-  drawer.addEventListener('click', () => {
-    menu.classList.toggle('active');
-  });
-
-   document.addEventListener('click', (event) => {
-    if (!menu.contains(event.target) && !drawer.contains(event.target)) {
-      menu.classList.remove('active');
-    }
-  });
-
-  const menuLinks = document.querySelectorAll('.menu a');
-
-  menuLinks.forEach(link => {
-    link.addEventListener('click', () => {
-      menu.classList.remove('active'); // Fecha o menu ao clicar em um link
+    document.addEventListener('click', (event) => {
+        if (!menu.contains(event.target) && !drawer.contains(event.target)) {
+            menu.classList.remove('active');
+        }
     });
-  });
-});
+
+    const menuLinks = document.querySelectorAll('.menu li a');
+
+    menuLinks.forEach(link => {
+        link.addEventListener('click', () => {
+        menu.classList.remove('active');
+        });
+    });
+
+    // efeito de digitação
+    const textElement = document.getElementById('typing-text');
+    const subtextElement = document.getElementById('typing-subtext');
+    const text = "Olá, sou Yuri Lima";
+    const subtext = "Desenvolvedor Mobile";
+    let index = 0;
+
+    function typeEffect() {
+        if (index < text.length) {
+            textElement.textContent += text.charAt(index);
+            index++;
+            setTimeout(typeEffect, 100); // Ajuste o tempo de espera entre cada caractere digitado (em milissegundos)
+        } else {
+          index = 0;
+          typeSubtextEffect();
+        }
+    }
+
+    function typeSubtextEffect() {
+        if (index < subtext.length) {
+            subtextElement.textContent += subtext.charAt(index);
+            index++;
+            setTimeout(typeSubtextEffect, 100);
+        }
+    }
+    
+    typeEffect();
+
+    // currículo
+    const downloadButton = document.getElementById('download-cv');
+
+    downloadButton.addEventListener('click', () => {
+        const pdfFilePath = 'assets/docs/Currículo.pdf';
+        const link = document.createElement('a');
+
+        link.href = pdfFilePath;
+        link.download = 'Curriculo.pdf';
+        link.target = '_blank'; // Abrir o link em uma nova aba (opcional)
+
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    });
+})
